3f2fbf62da17d1058f16e4d6429918208c9138de
Svenkiel <svenkiel2004@gmail.com>
2024-10-03 19:58:12 +0200

    Navbar aangemaakt en include toegevoegd aan de index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Home</title>
+</head>
+<body>
+
+    <?php include 'navbar.php'; ?>
+
+    <h1>Homepagina</h1>
+
+</body>
+</html>
