e7427178dbbb02eb094bf9b78098f3513802834c
David Klein <david.klein@student.nhlstenden.com>
2024-10-14 10:21:03 +0200

    Begin gemaakt aan homepagina en index.css toegevoegd.

--- index.php
+++ index.php
@@ -3,15 +3,46 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/index.css">
     <title>Home</title>
 </head>
 <body>
 
     <?php include 'nav/navbar.php'; ?>
 
-    <h1>Welkom op de homepagina</h1>
-    <p>Inhoud van de homepagina...</p>
+    <main>
+        <div class="home-top">
+            <img src="img/Sunny_socks_blue.jpg" alt="Sok" class="background">
+            <img src="img/sunny_logos_slogan_blue.png" alt="Sunny Socks" class="sunny-logo">
+            <p>Sunny Socks delivers high-quality, sustainable socks.
+            With fast shipping and an easy ordering process, Sunny Socks makes it convenient for customers to enjoy premium, 
+            sustainable products without delay, offering the perfect blend of quality, responsibility, and speed.</p>
+            <a href="catalogue.php" class="catalogue-button"><b>CATALOGUE</b></a>
+        </div>
+        <div class="home-middle">
+            <h1 class="home-title"><b>WHY ORDER FROM US?</b></h1>
+            <div class="punten">
+                <div class="een">
+                    <h1><b>01</b></h1>
+                    <h2><b>Fast Shipping</b></h2>
+                    <p>Socks Express Service is a fast and convenient solution for delivering high-quality socks directly to customers' doors.</p>
+                </div>
+                <div class="twee">
+                    <h1><b>02</b></h1>
+                    <h2><b>Easy and Fast Contact</b></h2>
+                </div>
+                <div class="drie">
+                    <h1><b>03</b></h1>
+                    <h2><b>Ethical and Sustainable Socks</b></h2>
+                </div>
+            </div>
+        </div>
+        <div class="home-bottom">
+            <h1 class="home-title"><b>TESTIMONIALS</b></h1>
+
+
+        </div>
+    </main>
 
 </body>
 </html>
