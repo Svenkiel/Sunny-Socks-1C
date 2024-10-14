156ee9b79017760d3feed711fe26d0e3f6c4e4af
Niki <niki.kuijpers@student.nhlstenden.com>
2024-10-14 10:53:27 +0200

    Div's toegevoegd (Test)

--- services.php
+++ services.php
@@ -11,38 +11,49 @@
     <?php include 'nav/navbar.php'; ?>
 
     <main>
-    <h1>Services</h1>
-     
-        <h3>Socks Express Service</h3>
-            <p> 
+        
+        <div class="title-container">
+            <h1 class="page-title">SERVICES</h1>
+        </div>
+
+        <div class="SES-container">
+            <h3>Socks Express Service</h3>
+            <p class="page-p"> 
                 Socks Express Service is a fast and convenient solution for 
                 delivering high-quality socks directly to customers' doors.
-            </p>   
+            </p>
+        </div>
+     
+        <div class="AIE-container">
+            <h3>Afterpay In Europe</h3>
+            <p class="page-p"> 
+                Afterpay is available as a payment option across Europe, 
+                offering customers a flexible and convenient way to shop.
+            </p>
+        </div>
 
-        <h3>Afterpay In Europe</h3>
-        <p> 
-            Afterpay is available as a payment option across Europe, 
-            offering customers a flexible and convenient way to shop.
-        </p>
+        <div class="HE-container">
+            <h3>High Ethics</h3>
+            <p class="page-p">
+                The company only partners with suppliers from countries 
+                that adhere to strict labor laws and ethical manufacturing 
+                standards. By conducting thorough audits and maintaining transparent 
+                relationships with its manufacturers, Sunny Socks guarantees 
+                that its production process supports fair wages, safe working conditions, 
+                and the well-being of all workers.
+            </p>
+        </div>
 
-        <h3>High Ethics</h3>
-        <p>
-            The company only partners with suppliers from countries 
-            that adhere to strict labor laws and ethical manufacturing 
-            standards. By conducting thorough audits and maintaining transparent 
-            relationships with its manufacturers, Sunny Socks guarantees 
-            that its production process supports fair wages, safe working conditions, 
-            and the well-being of all workers.
-        </p>
-
-        <h3>Easy Contact</h3>
-        <p> 
-            We prioritize clear and accessible communication, 
-            ensuring that contacting us is easy and hassle-free. 
-            Whether through phone, email, or live chat, 
-            our dedicated customer support team is always ready to assist 
-            with inquiries or concerns.
-        </p>
+        <div class="EC-container">
+            <h3>Easy Contact</h3>
+            <p class="page-p"> 
+                We prioritize clear and accessible communication, 
+                ensuring that contacting us is easy and hassle-free. 
+                Whether through phone, email, or live chat, 
+                our dedicated customer support team is always ready to assist 
+                with inquiries or concerns.
+            </p>
+        </div>
 
     </main>
     
