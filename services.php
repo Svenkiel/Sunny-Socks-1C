21f0c3571c5c36031591f07f6ba4da45a15fb9b2
Niki <niki.kuijpers@student.nhlstenden.com>
2024-10-14 09:50:03 +0200

    Tekst toegevoegd

--- services.php
+++ services.php
@@ -3,15 +3,49 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/services.css">
     <title>Services</title>
 </head>
 <body>
 
     <?php include 'nav/navbar.php'; ?>
 
-    <h1>Welkom op de Service pagina</h1>
-    <p>Inhoud van de servicepagina...</p>
+    <h1>Services</h1>
+
+    <main>
+        <h3>Socks Express Service</h3>
+        <p> 
+            Socks Express Service is a fast and convenient solution for 
+            delivering high-quality socks directly to customers' doors.
+        </p>
+
+        <h4>Afterpay In Europe</h4>
+        <p> 
+            Afterpay is available as a payment option across Europe, 
+            offering customers a flexible and convenient way to shop.
+        </p>
+
+        <h5>High Ethics</h5>
+        <p>
+            The company only partners with suppliers from countries 
+            that adhere to strict labor laws and ethical manufacturing 
+            standards. By conducting thorough audits and maintaining transparent 
+            relationships with its manufacturers, Sunny Socks guarantees 
+            that its production process supports fair wages, safe working conditions, 
+            and the well-being of all workers.
+        </p>
+
+        <h6>Easy Contact</h6>
+        <p> 
+            We prioritize clear and accessible communication, 
+            ensuring that contacting us is easy and hassle-free. 
+            Whether through phone, email, or live chat, 
+            our dedicated customer support team is always ready to assist 
+            with inquiries or concerns.
+        </p>
+
+    </main>
+    
 
 </body>
 </html>
