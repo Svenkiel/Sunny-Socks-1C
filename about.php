d13562aea14735cb7565de78b4e30dfb73cbdbb1
DuyguAtlas <duygu.atlas1@student.nhlstenden.com>
2024-10-22 13:00:28 +0200

    Aanpassing IMG
    In about us pagina

--- about.php
+++ about.php
@@ -11,9 +11,9 @@
     <?php include 'nav/navbar.php'; ?>
 
     <div class="aboutus-title">
-    <h1>ABOUT US</h1>
+    <h1 class>ABOUT US</h1>
+    
     </div>
-
 
         <div class="intro-container">
                 <div class="intro-text">
@@ -27,45 +27,43 @@
         </div>
 
 
-    <div class="grid-container">
-       
-        <div class="grid-1">
-            <h2>International Reach, Local Care</h2>
-            <p>With a focus on serving the global market, Sunny Socks ensures every order, no matter the location, arrives on time. Our logistics team handles shipping to major industrial hubs and remote work sites, delivering our socks to clients around the world.</p>
+<div class="grid-container">
+            <div class="grid-item">
+                <h2>International Reach, Local Care</h2>
+                <p>With a focus on serving the global market, Sunny Socks ensures every order, no matter the location, arrives on time. Our logistics team handles shipping to major industrial hubs and remote work sites, delivering our socks to clients around the world.</p>
+            </div>
 
-            <h2>Efficient Global Logistics</h2>
-            <p>Our state-of-the-art distribution center ensures that your orders are processed quickly and delivered globally. Sunny Socks is committed to providing seamless logistics solutions, with every box carefully prepared and shipped to your location, whether you’re based in Europe, Asia, or beyond.</p>
-        </div>
+            <div class="grid-item">
+                <h2>Efficient Global Logistics</h2>
+                <p>Our state-of-the-art distribution center ensures that your orders are processed quickly and delivered globally. Sunny Socks is committed to providing seamless logistics solutions, with every box carefully prepared and shipped to your location, whether you’re based in Europe, Asia, or beyond.</p>
+            </div>
+            <div class="grid-item">
+                    <h2>Socks Built for Performance</h2>
+                    <p>Sunny Socks offers a wide range of sturdy, professional-grade socks designed for tough environments. From the freezing conditions on oil rigs to the rugged demands of construction sites, our socks are crafted to support the daily work of employees across various industries. Our collections are specifically tailored for organizations that prioritize comfort, durability, and quality in their footwear.</p>
+                </div>
+                
+                <img src="img/sunny_socks_stock photo.png" class="img" alt="sock">
 
-        <hr>
-
-        <div class="grid-2">
-            <h2>Socks Built for Performance</h2>
-            <p>Sunny Socks offers a wide range of sturdy, professional-grade socks designed for tough environments. From the freezing conditions on oil rigs to the rugged demands of construction sites, our socks are crafted to support the daily work of employees across various industries. Our collections are specifically tailored for organizations that prioritize comfort, durability, and quality in their footwear.</p>
-
-            <img src="img/sunny_socks_stock photo.png" class="img">
-        </div>
-
-        <hr>
-
-        <div class="grid-3">
-            <img src="img/mil_socks.png" class="img">
-            <h2>Military-Grade Warmth 
-            and Comfort</h2>
-            <p>Our military edition socks are designed to meet the exacting standards of soldiers, air force personnel, and navy workers. Created with reinforced material for extra durability and warmth, these socks ensure that your personnel’s feet stay protected in the harshest environments. The minimal design, in camouflage and tactical colors, keeps things simple yet functional.</p>
-        </div>
-
-        <hr>
-
-        <div class="grid-4">
-            <img src="img/sunny_socks_packages.png" class="img">
-            <h2>Sustainable and Practical 
-            Packaging</h2>
-            <p>At Sunny Socks, we believe in sustainability. Our packaging is simple yet sturdy, designed to protect the products during transit while minimizing environmental impact. Each sock is folded neatly with a recycled cardboard holder, indicating size and brand, ready to ship directly to your business.</p>
-        </div>
+                <img src="img/mil_socks.png" class="img" alt="military socks">
+     
+            <div class="grid-item">
+                <h2>Military-Grade Warmth and Comfort</h2>
+                <p>Our military edition socks are designed to meet the exacting standards of soldiers, air force personnel, and navy workers. Created with reinforced material for extra durability and warmth, these socks ensure that your personnel’s feet stay protected in the harshest environments. The minimal design, in camouflage and tactical colors, keeps things simple yet functional.</p>
+            </div>
 
 
-    </div>
+            <div class="grid-item">
+                <h2>Sustainable and Practical Packaging</h2>
+                <p>At Sunny Socks, we believe in sustainability. Our packaging is simple yet sturdy, designed to protect the products during transit while minimizing environmental impact. Each sock is folded neatly with a recycled cardboard holder, indicating size and brand, ready to ship directly to your business.
+                </p>
+            </div>
+            
+            <img src="img/sunny_socks_packages.png" class="img" alt="packaging">
+
+
+        
+</div>
+
 
 </body>
 </html>
