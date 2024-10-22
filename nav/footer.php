eba883544c96f4ccdd2eeb3f2bf095a967265564
Niki <niki.kuijpers@student.nhlstenden.com>
2024-10-22 11:05:28 +0200

    Footer toevoegingen testen.

--- nav/footer.php
+++ nav/footer.php
@@ -3,40 +3,79 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/footer.css">
+    <link rel="stylesheet" href="css/footer.css" type="text/css">
     <title>Footer</title>
 </head>
 <body>
 
-<nav class="footer">
+<footer>
+        <div class="footer-container">
+            
+            <div class="footer-section">
+                <h2>Sunny.</h2>
+            </div>
 
-    <div class="logo">
-    <img src="img/logo.png" alt="Logo">
-    </div>
+            <div class="footer-links">
 
-    <div class= footer-ul>
-        <h3>LINKS</h3>
-        <ul>
-            <li><a href="index.php">Home</a></li>
-            <li><a href="catalogue.php">Shop</a></li>
-            <li><a href="about.php">About Us</a></li>
-            <li><a href="contact.php">Contact</a></li>
-        </ul>
+                <div class="footer-column">
+                    <h3>LINKS</h3>
+                    <ul>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Shop</a></li>
+                        <li><a href="#">About Us</a></li>
+                        <li><a href="#">Contact</a></li>
+                    </ul>
+                </div>
 
-        <h3>SHOP</h3>
-        <ul>
-            <li><a href="catalogue.php">Products</a></li>
-            <li><a href="cart.php">Overview</a></li>
-            <li><a href="catalogue.php">Pricing</a></li>
-            <li><a href="catalogue.php">Releases</a></li>
-        </ul>
+                <div class="footer-column">
+                    <h3>SHOP</h3>
+                    <ul>
+                        <li><a href="#">Products</a></li>
+                        <li><a href="#">Overview</a></li>
+                        <li><a href="#">Pricing</a></li>
+                        <li><a href="#">Releases</a></li>
+                    </ul>
+                </div>
 
-        <h3>CONTACT US</h3>
-        <ul>
-            <li><a href="contact.php">Telephone</a></li>
-            <li><a href="contact.php">Email</a></li>
-        </ul>
-</nav>
+                <div class="footer-column">
+                    <h3>CONTACT US</h3>
+                    <ul>
+                        <li><a href="#">Telephone</a></li>
+                        <li><a href="#">Email</a></li>
+                    </ul>
+                </div>
+
+                <div class="footer-column">
+                    <h3>OUR SOCIALS</h3>
+                    <div class="social-icons">
+                        <a href="#"><img src="img/footer/social-media.png" alt="Facebook"></a>
+                        <a href="#"><img src="img/footer/twitter.png" alt="Twitter"></a>
+                        <a href="#"><img src="img/footer/instagram.png" alt="Instagram"></a>
+                    </div>
+                    <div class="language-selector">
+                        <span>Language:</span>
+                        <img src="uk.png" alt="English">
+                        <img src="france.png" alt="French">
+                    </div>
+                </div>
+
+                <div class="footer-column">
+                    <h3>JOIN OUR MAILING LIST</h3>
+                    <form>
+                        <input type="email" placeholder="Email">
+                        <button type="submit">Subscribe</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+
+        <div class="footer-bottom">
+            <a href="#">Terms</a>
+            <a href="#">Privacy</a>
+            <a href="#">Cookies</a>
+        </div>
+    </footer>
     
 </body>
 </html>
+        
