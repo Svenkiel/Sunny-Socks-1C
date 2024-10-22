f18f3acea352a0cfd10d8bae8f0d949516162f5d
Niki <niki.kuijpers@student.nhlstenden.com>
2024-10-22 15:07:49 +0200

    test

--- nav/footer.php
+++ nav/footer.php
@@ -1,11 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/footer.css" type="text/css">
-    <title>Footer</title>
-</head>
 <body>
     <footer>
         <div class="footer-container">
@@ -74,6 +66,4 @@
             <a href="#">Cookies</a>
         </div>
     </footer>
-</body>
-</html>
-        
+</body>     
