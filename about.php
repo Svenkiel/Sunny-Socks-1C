71507c96e259da2c629350815188e05211ce058c
DuyguAtlas <duygu.atlas1@student.nhlstenden.com>
2024-10-24 13:19:51 +0200

    link stylesheest

--- about.php
+++ about.php
@@ -8,6 +8,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/about.css">
+    <link rel="stylesheet" href="css/footer.css">
+    <link rel="stylesheet" href="css/index.css">
     <title>About Us</title>
 </head>
 <body>
