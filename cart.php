5a6a0a8c4c9e2e87067499ba02ed68021544ea65
JoranVos <joran.vos@student.nhlstenden.com>
2024-10-24 13:33:21 +0200

    Footer toegevoegd aan de checkout en cart-pagina

--- cart.php
+++ cart.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/cart.css" type="text/css">
+    <link rel="stylesheet" href="css/footer.css" type="text/css"> 
     <title>Cart</title>
 </head>
 <body>
@@ -132,4 +133,7 @@
         }
     </script>
 </body>
+
+<?php include 'nav/footer.php'; ?>
+
 </html>
