b0c233bc058389c3edea0d9ebd8f0d11627701c7
Sven Kiel <svenkiel2004@gmail.com>
2024-10-25 09:59:24 +0200

    text/css toegevoegd aan de links

--- catalogue.php
+++ catalogue.php
@@ -3,8 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/catalogue.css">
-    <link rel="stylesheet" href="css/footer.css">
+    <link rel="stylesheet" href="css/catalogue.css" type="text/css">
+    <link rel="stylesheet" href="css/footer.css" type="text/css">
     <title>Catalogue</title>
 </head>
 <body>
